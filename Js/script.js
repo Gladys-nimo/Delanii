842d8a3ad95980a57c38d73d6c28c113a9d3712b
Gladys-nimo <wkanyora10@gmail.com>
2020-10-12 12:43:40 +0300

    added some script functions

---
+++ Js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design-image").slideDown().hide();
+        $("design").show();
+    });
+    $("#design").click(function(){
+        $("#design").slideUp();
+        $("design-image").slideDown();
+    });
+});
+
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design-image").slideDown().hide();
+        $("design").show();
+    });
+    $("#design").click(function(){
+        $("#design").slideUp();
+        $("design-image").slideDown();
+    });
+});
+
+$(document).ready(function(){
+    $("#design-image").click(function(){
+        $("#design-image").slideDown().hide();
+        $("design").show();
+    });
+    $("#design").click(function(){
+        $("#design").slideUp();
+        $("design-image").slideDown();
+    });
+});
+
+$(document).ready(function(){
+    $("work-one").mouseover(function(){
+        $("#img").show();
+        .mouseout(function(){
+            $("img").hide();
+        });
+
+    });
+});
