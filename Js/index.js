a73a8e5c4ae28f0ee7ba8640b55a0e1e0b38e445
Gladys-nimo <wkanyora10@gmail.com>
2020-10-14 11:22:18 +0300

    added some toggle effect

--- Js/index.js
+++ Js/index.js
@@ -1,6 +1,23 @@
-/ $(document).ready(function(){
-       $("#design").click(function() {
-          $(".hide").toggle();
-          $("#design-show").toggle();
-       });
-  
+$(document).ready(function() {
+
+   $(".clickable1").click(function() {
+         $(".hide1").toggle();
+         $("#design-show-image").toggle();
+      });
+      $(".clickable2").click(function(){
+         $("#develop-show-image").toggle();
+         $(".hide2").toggle();
+      });
+
+      $(".clickable3").click(function(){
+         $("#product-show-image").toggle();
+         $(".hide3").toggle();
+      });
+   });
+
+//$(document).ready(function() {//
+//    $(".clickable").click(function(){
+//       $("#develop-show-image").toggle();
+//       $(".hide").toggle();
+//    });
+// });
