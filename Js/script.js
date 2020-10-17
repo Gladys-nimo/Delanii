97b67ccca22d06a3d7c521c78b49ed86433229cc
Gladys-nimo <wkanyora10@gmail.com>
2020-10-17 13:17:11 +0300

    my message alert is working

--- Js/script.js
+++ Js/script.js
@@ -1,92 +1,119 @@
 
-$(document).ready(function(){
-   $("#work1").mouseover(function(){
-     $("#overlay1").show();
-   }).mouseout(function(){
-     $("#overlay1").hide();
-   });
- });
- $(document).ready(function(){
-   $("#work2").mouseover(function(){
-     $("#overlay2").show();
-   }).mouseout(function(){
-     $("#overlay2").hide();
-   });
- });
- $(document).ready(function(){
-   $("#work3").mouseover(function(){
-     $("#overlay3").show();
-   }).mouseout(function(){
-     $("#overlay3").hide();
-   });
- });
- $(document).ready(function(){
-   $("#work4").mouseover(function(){
-     $("#overlay4").show();
-   }).mouseout(function(){
-     $("#overlay4").hide();
-   });
- });
+// $(document).ready(function(){
+//    $("#work1").mouseover(function(){
+//      $("#overlay1").show();
+//    }).mouseout(function(){
+//      $("#overlay1").hide();
+//    });
+//  });
+//  $(document).ready(function(){
+//    $("#work2").mouseover(function(){
+//      $("#overlay2").show();
+//    }).mouseout(function(){
+//      $("#overlay2").hide();
+//    });
+//  });
+//  $(document).ready(function(){
+//    $("#work3").mouseover(function(){
+//      $("#overlay3").show();
+//    }).mouseout(function(){
+//      $("#overlay3").hide();
+//    });
+//  });
+//  $(document).ready(function(){
+//    $("#work4").mouseover(function(){
+//      $("#overlay4").show();
+//    }).mouseout(function(){
+//      $("#overlay4").hide();
+//    });
+//  });
  
- $(document).ready(function(){
-   $("#work5").mouseover(function(){
-     $("#overlay5").show();
-   }).mouseout(function(){
-     $("#overlay5").hide();
-   });
-   $("#work6").mouseover(function(){
-     $("#overlay6").show();
-   }).mouseout(function(){
-     $("#overlay6").hide();
-   });
-   $("#work7").mouseover(function(){
-     $("#overlay7").show();
-   }).mouseout(function(){
-     $("#overlay7").hide();
-   });
-   $("#work8").mouseover(function(){
-     $("#overlay8").show();
-   }).mouseout(function(){
-     $("#overlay8").hide();
-   });
-   const myForm = documentgetElementById("myForm") 
-   myForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert(a + "	we have received your message. Thank you for reaching out to us");
-    //  $('form').on('​.onSubmit',function(){ alert('submitted'); });. 
-  //  $(document).ready(function(){
-  //   $("form#form34A").submit(function(event){
-  //     // event.preventDefault();
-  //     var name = $("input#merge1").val();
-  //     var email = $("input#merge0").val();
-  //     var message = $("textarea#comment").val();
-  //     if ($("input#merge1").val() && $("input#merge0").val()){
-  //       alert (name + ", we have received your message. Thank you for reaching out to us.");
-  //     }
-  //     else {
-  //       alert("Please enter your name and email!");
-  //     }
-      
-    });
-   // $(document).ready(function(){
-   //    $(".design-image").click(function() {
-   //       $(".hide").toggle();
-   //       $("design-show").toggle(;
-   //    })
-   // })
-  //  $(document).ready(function(){
-  //   $("form#form34A").submit(function(event){
-  //     // event.preventDefault();
-  //     var name = $("input#merge1").val();
-  //     var email = $("input#merge0").val();
-  //     var message = $("textarea#comment").val();
-  //     if ($("input#merge1").val() && $("input#merge0").val()){
-  //       alert (name + ", we have received your message. Thank you for reaching out to us.");
-  //     }
-  //     else {
-  //       alert("Please enter your name and email!");
-  //     }
-      
-  //   });
+//  $(document).ready(function(){
+//    $("#work5").mouseover(function(){
+//      $("#overlay5").show();
+//    }).mouseout(function(){
+//      $("#overlay5").hide();
+//    });
+//    $("#work6").mouseover(function(){
+//      $("#overlay6").show();
+//    }).mouseout(function(){
+//      $("#overlay6").hide();
+//    });
+//    $("#work7").mouseover(function(){
+//      $("#overlay7").show();
+//    }).mouseout(function(){
+//      $("#overlay7").hide();
+//    });
+//    $("#work8").mouseover(function(){
+//      $("#overlay8").show();
+//    }).mouseout(function(){
+//      $("#overlay8").hide();
+//    });
    
- });
+  
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
